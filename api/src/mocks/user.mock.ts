22ba7b1bff9bb044464c2bf4e429e820e752fa9c
SilvioDev26 <Silviodev102009@gmail.com>
2026-08-30 14:46:07 +0000

    feat: adicionar dados simulados

---
+++ api/src/mocks/user.mock.ts
@@ -0,0 +1,20 @@
+import type { User } from '../types/user.type.ts';
+
+const users: User[] = [{
+    id:1,
+    name: 'Silvio',
+    email: 'Silvio@email.com',
+    password: 'Sg124232',
+},{
+    id: 2,
+    name: 'fernando',
+    email:'fernado@gmail.com',
+    password: 'fg121212',
+},{
+    id:3,
+    name: 'gabi',
+    email: 'gabi@email.com',
+    password: 'asaa1313',
+}];
+
+export default users;
