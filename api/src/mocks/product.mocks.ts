f15ae2011fc1bc226874811fa955870dfacfc3e7
SilvioDev26 <Silviodev102009@gmail.com>
2026-09-20 14:34:58 +0000

    feat:iniciar aplicacao de produtos para exercicios

---
+++ api/src/mocks/product.mocks.ts
@@ -0,0 +1,15 @@
+import type {Product} from '../types/product.type.ts';
+
+const products: Product[] = [{
+    id: 1,
+    name: 'Juice',
+    price: 39.90,
+    description: null
+},{
+id: 2,
+name: 'Juice',
+price: 7.99,
+description:null
+}]
+
+export default products;
